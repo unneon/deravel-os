71c9791cc9f2a9786368a88b86706d6521dca214
Mateusz Cegiełka <mateusz@cegla.net>
2026-06-06 13:35:36 +0200

    Add terminal text rendering

--- apps/build.rs
+++ apps/build.rs
@@ -1,4 +1,37 @@
+use fontdue::{Font, FontSettings};
+use std::fmt::Write;
+
 fn main() {
     println!("cargo::rerun-if-changed=../kernel-api/user.ld");
     println!("cargo::rustc-link-arg=-Tkernel-api/user.ld");
+
+    let font = include_bytes!("/usr/share/fonts/TTF/FiraCode-Regular.ttf");
+    let font = Font::from_bytes(font.as_slice(), FontSettings::default()).unwrap();
+    let mut out = String::new();
+    writeln!(
+        out,
+        r#"struct Character {{
+    ascii: u8,
+    xmin: i32,
+    ymin: i32,
+    width: usize,
+    height: usize,
+    bitmap: &'static [u8],
+}}
+
+static FONT: [Character; 94] = ["#
+    )
+    .unwrap();
+    for c in u8::MIN..=u8::MAX {
+        if c.is_ascii_graphic() {
+            let (metrics, bitmap) = font.rasterize(c as char, 17.0);
+            writeln!(out, "Character {{ ascii: {c}, xmin: {}, ymin: {}, width: {}, height: {}, bitmap: &{bitmap:?} }},", metrics.xmin, metrics.ymin, metrics.width, metrics.height).unwrap();
+        }
+    }
+    writeln!(out, "];").unwrap();
+    std::fs::write(
+        format!("{}/font.rs", std::env::var("OUT_DIR").unwrap()),
+        out,
+    )
+    .unwrap();
 }
--- apps/src/bin/terminal.rs
+++ apps/src/bin/terminal.rs
@@ -1,24 +1,104 @@
 #![no_std]
 #![no_main]
 
+include!(concat!(env!("OUT_DIR"), "/font.rs"));
+
 use deravel_kernel_api::*;
+use rand::{RngExt, SeedableRng};
+
+struct Renderer<'a> {
+    cursor_x: i32,
+    cursor_y: i32,
+    window_width: i32,
+    window_height: i32,
+    framebuffer: &'a mut [u8],
+    window: Capability<Window>,
+}
+
+impl Renderer<'_> {
+    fn render_char(&mut self, c: u8) {
+        if c == b' ' {
+            self.cursor_x += 10;
+            return;
+        } else if c == b'\n' {
+            self.cursor_x = 0;
+            self.cursor_y += 17;
+            return;
+        } else if let Some(glyph) = FONT.iter().find(|glyph| glyph.ascii == c) {
+            for bitmap_y in 0..glyph.height as i32 {
+                for bitmap_x in 0..glyph.width as i32 {
+                    let fb_x = self.cursor_x + bitmap_x + glyph.xmin;
+                    let fb_y = self.cursor_y + 17 - glyph.height as i32 + bitmap_y - glyph.ymin;
+                    if fb_x >= 0
+                        && fb_x < self.window_width
+                        && fb_y >= 0
+                        && fb_y < self.window_height
+                    {
+                        let [b, g, r, _] = &mut self.framebuffer.as_chunks_mut().0
+                            [fb_y as usize * self.window_width as usize + fb_x as usize];
+                        let color =
+                            glyph.bitmap[bitmap_y as usize * glyph.width + bitmap_x as usize];
+                        *b = 0;
+                        *g = color;
+                        *r = 0;
+                    }
+                }
+            }
+            self.cursor_x += glyph.width as i32;
+        }
+
+        if self.cursor_x + 10 > self.window_width {
+            self.cursor_x = 0;
+            self.cursor_y += 17;
+        }
+        if self.cursor_y + 17 > self.window_height {
+            self.cursor_x = 0;
+            self.cursor_y = 0;
+            self.clear_screen();
+        }
+        self.window.draw();
+    }
+
+    fn clear_screen(&mut self) {
+        for [b, g, r, a] in self.framebuffer.as_chunks_mut().0 {
+            *b = 0;
+            *g = 0;
+            *r = 0;
+            *a = 255;
+        }
+    }
+}
 
 fn main(args: Args) {
     let window = args.windowing.create_window();
     let framebuffer = window.framebuffer();
     let (framebuffer_ptr, framebuffer_len) = unsafe { syscall::map_shared_memory(framebuffer) };
     let framebuffer = unsafe { core::slice::from_raw_parts_mut(framebuffer_ptr, framebuffer_len) };
+    let mut renderer = Renderer {
+        cursor_x: 0,
+        cursor_y: 0,
+        window_width: window.width() as i32,
+        window_height: window.height() as i32,
+        framebuffer,
+        window,
+    };
 
-    for [b, g, r, a] in framebuffer.as_chunks_mut().0 {
-        *b = 5;
-        *g = 22;
-        *r = 37;
-        *a = 255;
+    renderer.clear_screen();
+    for &c in "Hello, world!\nuwu".as_bytes() {
+        renderer.render_char(c);
     }
 
-    window.draw();
-
+    let start_time = riscv::register::time::read();
+    let timebase_frequency = unsafe { syscall::riscv_timebase_frequency() } as f64;
+    let mut last_time = 0.;
+    let mut rng = rand::rngs::SmallRng::seed_from_u64(907);
     loop {
+        let time = (riscv::register::time::read() - start_time) as f64 / timebase_frequency;
+        if time - last_time > 0.1 {
+            renderer.render_char(rng.random_range(b'a'..=b'z'));
+            last_time += 0.1;
+        }
+
         yield_();
     }
 }
